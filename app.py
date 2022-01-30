d7c762321c16d2c43e50cae81ec989877f2671ec
Tinashe Kachikoti <tinashekachikoti@gmail.com>
2022-01-30 16:29:34 +0000

    add jinja template page to organise page creation

--- app.py
+++ app.py
@@ -8,9 +8,21 @@ APP = Flask(__name__)
 def index():
     components_csv = FlatFileDatabase('models/components.csv')
     return render_template(
-        'index.html',
+        'index.html.jinja',
         page_title = 'Home',
         components_csv = components_csv.get_table_rows())
+
+@APP.route('/create')
+def create():
+    components_csv = FlatFileDatabase('models/components.csv')
+    priority_and_severity_options_csv = FlatFileDatabase(
+        'models/priority_and_severity_options.csv')
+    return render_template(
+        'create.html.jinja',
+        page_title = 'Create Ticket',
+        components_csv = components_csv.get_table_rows(),
+        priority_and_severity_options_csv = (
+            priority_and_severity_options_csv.get_table_rows()))
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=3000, debug=True)
