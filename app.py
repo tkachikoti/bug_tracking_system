ae2aaa66ef91053b32de84e12773cd1c9ecca2dc
Tinashe Kachikoti <tinashekachikoti@gmail.com>
2022-02-01 14:08:36 +0000

    reorganised file structure

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 
 from flat_file_database import FlatFileDatabase
 
-app = Flask(__name__)
+app = Flask(__name__, root_path='flaskr')
 
 @app.route('/')
 def index():
@@ -10,7 +10,7 @@ def index():
         'index.html.jinja',
         page_title = 'Home',
         tickets_cvs = FlatFileDatabase(
-            'models/tickets.csv').select_all_rows_on_csv())
+            'flaskr/models/tickets.csv').select_all_rows_on_csv())
 
 @app.route('/create', methods=['GET', 'POST'])
 def create():
@@ -20,7 +20,7 @@ def create():
         component_name = request.form['component_name']
         description = request.form['description']
         # Validate the data
-        FlatFileDatabase('models/tickets.csv').modify_row_on_csv({
+        FlatFileDatabase('flaskr/models/tickets.csv').modify_row_on_csv({
             'component_name': request.form['component_name'],
             'title': request.form['title'],
             'description': request.form['description'],
@@ -34,16 +34,16 @@ def create():
             'create.html.jinja',
             page_title = 'Create Ticket',
             components_csv = FlatFileDatabase(
-                'models/components.csv').select_all_rows_on_csv(),
+                'flaskr/models/components.csv').select_all_rows_on_csv(),
             priority_and_severity_options_csv = (FlatFileDatabase(
-                'models/priority_and_severity_options.csv').select_all_rows_on_csv()),
+                'flaskr/models/priority_and_severity_options.csv').select_all_rows_on_csv()),
             status_options_csv = (FlatFileDatabase(
-                'models/status_options.csv').select_all_rows_on_csv()))
+                'flaskr/models/status_options.csv').select_all_rows_on_csv()))
 
 @app.route('/delete')
 def delete():
     if request.args['uid']:
-        FlatFileDatabase('models/tickets.csv').modify_row_on_csv({
+        FlatFileDatabase('flaskr/models/tickets.csv').modify_row_on_csv({
             'uid': request.args['uid']}, 'delete')
         # Redirect to the home page
         return redirect(url_for('index'))
